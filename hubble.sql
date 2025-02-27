449e19fbb4cd431c512457660148ccfdc6aef2ac
angie-xiao <angie.a.xiao@gmail.com>
2025-02-27 12:55:46 -0800

    use D_UNIFIED_CUST_SHIPMENT_ITEMS instead of order_items table

--- hubble.sql
+++ hubble.sql
@@ -1,45 +1,60 @@
+/*+ETLM {
+	depend:{
+		replace:[
+			{name:"andes.booker.D_MP_ASIN_MANUFACTURER", age:{days:1}},
+			{name:"andes.booker.D_UNIFIED_CUSTOMER_ORDER_ITEMS", age:{days:1}}
+			{name:"andes.contribution_ddl.O_WBR_CP_NA", age:{days:1}}
+		]
+	}
+}*/
+
+
+
+
 DROP TABLE IF EXISTS orders;
 CREATE TEMP TABLE orders AS (
-    SELECT
-        asin,
-        customer_id,
-        order_day,
-        region_id,
-        marketplace_id
-    FROM andes.booker.D_UNIFIED_CUSTOMER_ORDER_ITEMS o
+    SELECT DISTINCT  
+    	customer_shipment_item_id, 
+    	asin, 
+    	customer_id, 
+    	ship_day
+    FROM andes.booker.D_UNIFIED_CUST_SHIPMENT_ITEMS o
     WHERE o.region_id=1
         and o.marketplace_id = 7
-        and o.is_retail_order_item = 'Y'
-        and o.order_day BETWEEN TO_DATE('{RUN_DATE_YYYYMMDD}','YYYYMMDD')-180 AND TO_DATE('{RUN_DATE_YYYYMMDD}','YYYYMMDD')
-        and o.order_item_level_condition != 6
+        and o.is_retail_merchant = 'Y'
+   		and o.ship_day BETWEEN TO_DATE('{RUN_DATE_YYYY-MM-DD}', 'YYYY-MM-DD') - interval '120 days' and TO_DATE('{RUN_DATE_YYYY-MM-DD}', 'YYYY-MM-DD')
+        and o.order_condition != 6
 );
+
 
 
 DROP TABLE IF EXISTS orders_manu;
 CREATE TEMP TABLE orders_manu AS (
     SELECT
-        o.asin,
+        o.customer_shipment_item_id,
+    	o.asin,
         o.customer_id,
-        o.marketplace_id,
-        o.order_day,
+    	o.ship_day,
         m.dama_mfg_vendor_code,
         m.dama_mfg_vendor_name,
         m.brand_name,
         m.brand_code
     FROM orders o
-        left join andes.booker.d_mp_asin_manufacturer m
-        ON o.region_id = m.region_id
-        AND o.marketplace_id = m.marketplace_id
-        AND o.asin = m.asin
+        INNER JOIN andes.booker.d_mp_asin_manufacturer m
+        ON o.asin = m.asin
+   WHERE m.region_id = 1 AND m.marketplace_id=7
 );
+
+
 
 
 DROP TABLE IF EXISTS order_metrics;
 CREATE TEMP TABLE order_metrics AS (
     SELECT
         o.asin,
-        customer_id,
-        order_day,
+        o.customer_shipment_item_id,
+        o.customer_id,
+        o.ship_day,
         dama_mfg_vendor_code,
         dama_mfg_vendor_name,
         brand_name,
@@ -50,9 +65,13 @@ CREATE TEMP TABLE order_metrics AS (
         cp.display_ads_amt,
         cp.subscription_revenue_amt
     FROM orders_manu o
-        left join andes.contribution_ddl.O_WBR_CP_NA cp 
-        ON o.marketplace_id = cp.marketplace_id
-        AND o.asin = cp.asin
+        left join andes.contribution_ddl.O_WBR_CP_NA cp     
+        ON o.ship_day = cp.ship_day
+        AND o.customer_shipment_item_id = cp.customer_shipment_item_id 
+        AND o.asin = cp.asin 
+    WHERE cp.marketplace_id = 7 
+        AND cp.ship_day BETWEEN TO_DATE(cp.ship_day, 'YYYY-MM-DD') - interval '120 days' and TO_DATE(cp.ship_day, 'YYYY-MM-DD')
+		AND cp.marketplace_id=7
 );
 
 
@@ -65,15 +84,17 @@ CREATE TEMP TABLE cte1 AS (
         brand_name,
         asin,
         customer_id,
-        order_day,
+        ship_day,
         revenue_share_amt,
         display_ads_amt,
         subscription_revenue_amt,
-        LAG(order_day) OVER (PARTITION BY dama_mfg_vendor_code, customer_id ORDER BY order_day) AS last_purchase_date,
-        LAG(ASIN) OVER (PARTITION BY dama_mfg_vendor_code, customer_id ORDER BY order_day) AS last_purchase_asin
+        LAG(ship_day) OVER (PARTITION BY dama_mfg_vendor_code, customer_id ORDER BY ship_day) AS last_purchase_date,
+        LAG(ASIN) OVER (PARTITION BY dama_mfg_vendor_code, customer_id ORDER BY ship_day) AS last_purchase_asin
     FROM order_metrics
     WHERE dama_mfg_vendor_code != 'NaN'
 );
+
+
 
 DROP TABLE IF EXISTS cte2;
 CREATE TEMP TABLE cte2 AS (
@@ -84,7 +105,7 @@ CREATE TEMP TABLE cte2 AS (
         brand_code,
         brand_name,
         customer_id,
-        order_day,
+        ship_day,
         -- is_sns,
         -- prime_member_type,
         revenue_share_amt,
@@ -92,19 +113,21 @@ CREATE TEMP TABLE cte2 AS (
         subscription_revenue_amt,
         last_purchase_asin,
         last_purchase_date,
-        (
+        ( 
             CASE
                 WHEN last_purchase_date IS NULL THEN 'first purchase'
-                WHEN TO_DATE(last_purchase_asin,'YYYYMMDD') BETWEEN TO_DATE(order_day,'YYYYMMDD') - 30 AND TO_DATE(order_day,'YYYYMMDD') - 1 THEN 'return in 1 mo'
-                WHEN TO_DATE(last_purchase_asin,'YYYYMMDD') BETWEEN TO_DATE(order_day,'YYYYMMDD') - 60 AND TO_DATE(order_day,'YYYYMMDD')- 31 THEN 'return in 2 mo'
-                WHEN TO_DATE(last_purchase_asin,'YYYYMMDD') BETWEEN TO_DATE(order_day,'YYYYMMDD')  - 90 AND TO_DATE(order_day,'YYYYMMDD') - 61 THEN 'return in 3 mo'
-                WHEN TO_DATE(order_day,'YYYYMMDD') - TO_DATE(last_purchase_date,'YYYYMMDD') > 90 THEN 'return after 3 mo+'
+                WHEN TO_DATE(last_purchase_asin, 'YYYY-MM-DD') BETWEEN TO_DATE(ship_day,'YYYY-MM-DD') - interval '30 days' AND TO_DATE(ship_day,'YYYY-MM-DD') - interval '1 days' THEN 'return in 1 mo'
+                WHEN TO_DATE(last_purchase_asin,'YYYY-MM-DD') BETWEEN TO_DATE(ship_day,'YYYY-MM-DD') - interval '60 days' AND TO_DATE(ship_day,'YYYY-MM-DD')- interval '31 days' THEN 'return in 2 mo'
+                WHEN TO_DATE(last_purchase_asin,'YYYYMMDD') BETWEEN TO_DATE(ship_day,'YYYY-MM-DD')  - interval '90 days' AND TO_DATE(ship_day, 'YYYY-MM-DD') -  interval '61 days' THEN 'return in 3 mo'
+                WHEN TO_DATE(ship_day,'YYYY-MM-DD') - TO_DATE(last_purchase_date,'YYYY-MM-DD') > interval '90 days' THEN 'return after 3 mo+'
                 ELSE '/'
             END
         ) AS last_purchase_n_days_ago
     FROM  cte1
 );
 
+
+GRANT ALL ON TABLE caism.new_to_brand_job_test2 TO PUBLIC;
 DROP TABLE IF EXISTS CAISM.new_to_brand_job_test2;
 CREATE TABLE CAISM.new_to_brand_job_test2 AS (
     SELECT
@@ -114,7 +137,7 @@ CREATE TABLE CAISM.new_to_brand_job_test2 AS (
         brand_name,
         brand_code,
         customer_id,
-        order_day,
+        ship_day,
         last_purchase_asin,
         last_purchase_date,
         last_purchase_n_days_ago,
@@ -127,7 +150,7 @@ CREATE TABLE CAISM.new_to_brand_job_test2 AS (
         dama_mfg_vendor_code,
         asin,
         last_purchase_n_days_ago,
-        order_day,
+        ship_day,
         last_purchase_asin,
         last_purchase_date,
         -- is_sns,
@@ -141,4 +164,3 @@ CREATE TABLE CAISM.new_to_brand_job_test2 AS (
         last_purchase_n_days_ago ASC
 );
 
-GRANT ALL ON TABLE caism.new_to_brand_job_test2 TO PUBLIC;
