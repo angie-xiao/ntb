feeb9ba42cfc917dad15c7746fc3d3b73a42b636
angie-xiao <angie.a.xiao@gmail.com>
2025-05-07 21:35:31 -0500

    fixed deal joins

--- hubble.sql
+++ hubble.sql
@@ -42,8 +42,6 @@ CREATE TEMP TABLE orders_tmp AS (
         ON maa.asin = o.asin
         AND maa.marketplace_id=o.marketplace_id
         AND maa.region_id = o.region_id
-        -- LEFT JOIN andes.booker.d_mp_asin_cats c
-        -- on c.product_category = maa.product_category
         LEFT JOIN andes.booker.d_mp_asin_manufacturer m
         ON o.asin = m.asin
     WHERE o.region_id=1
@@ -62,9 +60,6 @@ CREATE TEMP TABLE orders_tmp AS (
             'gl_personal_care_appliances'
         )
         and o.customer_purchase_id not in ('-1', '?')
-        -- and m.dama_mfg_vendor_code != 'NaN'
-        -- and o.customer_id=756758481
-    -- GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
 );
 
 DROP TABLE IF EXISTS orders;
@@ -73,134 +68,246 @@ CREATE TEMP TABLE orders AS (
         t1.marketplace_id,
         t1.customer_shipment_item_id,
         t1.customer_purchase_id,
-    	t1.asin, 
+        t1.asin, 
         t1.gl_product_group_desc,
-    	t1.customer_id, 
+        t1.customer_id, 
         t1.order_date,
-        -- t1.dama_mfg_vendor_code,
-        -- t1.dama_mfg_vendor_name,
         t1.brand_name,
         t1.brand_code,
         MAX(t1.shipped_units) as shipped_units,
-        -- max(t1.subtotal) as subtotal,
-        -- t1.our_price_discount_amt as total_discount,
         MAX(t2.description) as product_category_desc
     FROM orders_tmp t1
         left join andes.booker.d_mp_asin_cats t2
         on t1.product_category = t2.product_category
-    GROUP BY 1,2,3,4,5,6,7,8,9
+    GROUP BY 
+        t1.marketplace_id,
+        t1.customer_shipment_item_id,
+        t1.customer_purchase_id,
+        t1.asin, 
+        t1.gl_product_group_desc,
+        t1.customer_id, 
+        t1.order_date,
+        t1.brand_name,
+        t1.brand_code
 );
 
 -- CP metrics
 -- purchase timeline flag
-DROP TABLE IF EXISTS order_metrics_tmp;
-CREATE TEMP TABLE order_metrics_tmp AS (
-    SELECT
-        o.marketplace_id,
-        o.customer_shipment_item_id,
-        o.customer_purchase_id,
-        o.asin,
-        o.gl_product_group_desc,
-        o.customer_id,
-        o.order_date,
-        o.brand_name,
-        o.brand_code,
-        o.product_category_desc,
-        sum(o.shipped_units) as shipped_units,
-        sum(cp.revenue_share_amt) as revenue_share_amt,
-        sum(cp.display_ads_amt) as display_ads_amt,
-        sum(cp.subscription_revenue_amt) as subscription_revenue_amt
-    FROM orders o
-        LEFT JOIN andes.contribution_ddl.O_WBR_CP_NA cp
-        ON o.customer_shipment_item_id = cp.customer_shipment_item_id 
-        AND o.asin = cp.asin
-    GROUP BY 
-        o.marketplace_id,  /* Added table qualifier */
-        o.customer_shipment_item_id,
-        o.customer_purchase_id,
-        o.asin,
-        o.gl_product_group_desc,
-        o.customer_id,
-        o.order_date,
-        o.brand_name,
-        o.brand_code,
-        o.product_category_desc
-);
-
-
 DROP TABLE IF EXISTS order_metrics;
 CREATE TEMP TABLE order_metrics AS (
-    select
-        o.*,
-        TO_DATE(LAG(o.order_date) OVER (PARTITION BY o.brand_code, o.customer_id ORDER BY o.order_date),'YYYY-MM-DD') AS last_purchase_date,
-        LAG(o.ASIN) OVER (PARTITION BY o.brand_code, o.customer_id ORDER BY o.order_date) AS last_purchase_asin,
+    with cte1 as (
+        SELECT
+            o.marketplace_id,
+            o.customer_shipment_item_id,
+            o.customer_purchase_id,
+            o.asin,
+            o.gl_product_group_desc,
+            o.customer_id,
+            o.order_date,
+            o.brand_name,
+            o.brand_code,
+            o.product_category_desc,
+            sum(o.shipped_units) as shipped_units,
+            sum(cp.revenue_share_amt) as revenue_share_amt,
+            sum(cp.display_ads_amt) as display_ads_amt,
+            sum(cp.subscription_revenue_amt) as subscription_revenue_amt
+        FROM orders o
+            LEFT JOIN andes.contribution_ddl.O_WBR_CP_NA cp
+            ON o.customer_shipment_item_id = cp.customer_shipment_item_id 
+            AND o.asin = cp.asin
+        GROUP BY 
+            o.marketplace_id, 
+            o.customer_shipment_item_id,
+            o.customer_purchase_id,
+            o.asin,
+            o.gl_product_group_desc,
+            o.customer_id,
+            o.order_date,
+            o.brand_name,
+            o.brand_code,
+            o.product_category_desc
+    ),
+    cte2 AS (
+        SELECT
+            cte1.*,
+            LAG(order_date) OVER (PARTITION BY brand_code, customer_id ORDER BY order_date) AS last_purchase_date
+        FROM cte1
+    )
+
+    SELECT
+        cte2.*,
         (CASE WHEN last_purchase_date IS NULL THEN 1 ELSE 0 END) as first_purchase_flag,
-        (CASE WHEN TO_DATE(last_purchase_date, 'YYYY-MM-DD') BETWEEN TO_DATE(order_date,'YYYY-MM-DD') - interval '30 days' AND TO_DATE(order_date,'YYYY-MM-DD') - interval '1 day' THEN 1 ELSE 0 END) one_mo_return_flag,
-        (CASE WHEN TO_DATE(last_purchase_date, 'YYYY-MM-DD') BETWEEN TO_DATE(order_date,'YYYY-MM-DD') - interval '60 days' AND TO_DATE(order_date,'YYYY-MM-DD') - interval '31 day' THEN 1 ELSE 0 END) two_mo_return_flag,
-        (CASE WHEN TO_DATE(last_purchase_date, 'YYYY-MM-DD') BETWEEN TO_DATE(order_date,'YYYY-MM-DD') - interval '90 days' AND TO_DATE(order_date,'YYYY-MM-DD') - interval '61 day' THEN 1 ELSE 0 END) third_mo_return_flag,
-        (CASE WHEN TO_DATE(last_purchase_date, 'YYYY-MM-DD') < TO_DATE(order_date,'YYYY-MM-DD') - interval '90 days' THEN 1 ELSE 0 END) as after_three_mo_return_flag
-    from order_metrics_tmp o
+        (CASE WHEN last_purchase_date BETWEEN order_date - interval '30 days' AND order_date - interval '1 day' THEN 1 ELSE 0 END) one_mo_return_flag,
+        (CASE WHEN last_purchase_date BETWEEN order_date - interval '60 days' AND order_date - interval '31 day' THEN 1 ELSE 0 END) two_mo_return_flag,
+        (CASE WHEN last_purchase_date BETWEEN order_date - interval '90 days' AND order_date - interval '61 day' THEN 1 ELSE 0 END) third_mo_return_flag,
+        (CASE WHEN last_purchase_date < order_date - interval '90 days' THEN 1 ELSE 0 END) three_mo_plus_return_flag
+    from cte2
 );
- -------------------------------------promotion / benchmark-------------------------------------------------------
--- do promotion stuff first. everything else is non-promo
+
+
+-------------------------------------------------- PROMOS --------------------------------------------------
 -- promotion
 -- + asin pricing
-DROP TABLE IF EXISTS array_data;
-CREATE TEMP TABLE array_data as (
-    select asin, split_to_array(purchase_order_ids , ',') as order, coupon_quantity
-    from andes.pdm.DIM_PROMOTION_ASIN
-    where purchase_order_ids is not null
-);
+-- DROP TABLE IF EXISTS array_data;
+-- CREATE TEMP TABLE array_data as (
+--     select asin, promotion_key, split_to_array(purchase_order_ids , ',') as order, coupon_quantity
+--     from andes.pdm.DIM_PROMOTION_ASIN
+--     where purchase_order_ids is not null
+--         and marketplace_key=7
+-- );
 
-DROP TABLE IF EXISTS asin_promos;
-CREATE TEMP TABLE asin_promos as (
-    select t.asin, coupon_quantity, orders as promo_purchase_id
-    from array_data as t
-        left join t.order as orders 
-        on True
-);
+-- DROP TABLE IF EXISTS asin_promos;
+-- CREATE TEMP TABLE asin_promos as (
+--     select t.asin, promotion_key, coupon_quantity, orders as promo_purchase_id
+--     from array_data as t
+--         left join t.order as orders 
+--         on True
+-- );
 
-DROP TABLE IF EXISTS asin_promos_cp;
+DROP TABLE IF EXISTS asin_promos_cp;    
 CREATE TEMP TABLE asin_promos_cp as (
     SELECT
+        t1.marketplace_key,
         t3.customer_shipment_item_id,
+        t3.customer_purchase_id,  
         t1.order_id,  
+        t1.promotion_key,
         t3.asin,
         t3.customer_id,
-        t2.coupon_quantity,
-        t1.units_shipped,
-        t1.product_gms
-    FROM andes.pdm.FACT_PROMOTION_CP t1
-        LEFT JOIN asin_promos t2
-        ON t2.asin=t1.asin
-        and t1.order_id=t2.promo_purchase_id
-        RIGHT JOIN order_metrics t3
+        t4.promotion_type,
+        t4.deal_first_available_datetime,
+        (CASE 
+            WHEN t4.deal_first_available_datetime is null
+            THEN 'N'
+            ELSE 'Y'
+        END) as is_deal_purchase,
+        t4.event_name,
+        t3.gl_product_group_desc,
+        t3.brand_name,
+        t3.brand_code,
+        t3.product_category_desc,
+        t3.first_purchase_flag,
+        t3.one_mo_return_flag,
+        t3.two_mo_return_flag,
+        t3.third_mo_return_flag,
+        t3.three_mo_plus_return_flag,
+        -- t2.coupon_quantity,
+        t3.shipped_units as shipped_units,
+        t3.revenue_share_amt as revenue_share_amt,
+        t3.display_ads_amt as display_ads_amt,
+        t3.subscription_revenue_amt as subscription_revenue_amt
+    FROM order_metrics t3
+        LEFT JOIN andes.pdm.FACT_PROMOTION_CP t1
         on t3.asin=t1.asin
         and t3.customer_shipment_item_id = t1.customer_shipment_item_id
         and t3.customer_id=t1.customer_id
         and t1.marketplace_key=t3.marketplace_id
+        -- LEFT JOIN asin_promos t2
+        -- ON t2.asin=t1.asin
+        -- and t1.order_id=t2.promo_purchase_id -- ok to join?
+        LEFT JOIN andes.pdm.DIM_PROMOTION t4
+        on t4.marketplace_key=t1.marketplace_key
+        and t4.promotion_key = t1.promotion_key
+        -- and t1.promo_asin = t4.
     WHERE t1.marketplace_key=7
-
 );
 
--- if not promotion then...
-DROP TABLE IF EXISTS flag_promo;
-CREATE TEMP TABLE flag_promo as (
-    SELECT 
-        *,
+-- Calculate event-level detail separately
+-- consolidating to asin level
+DROP TABLE IF EXISTS asin_purchase;
+CREATE TEMP TABLE asin_purchase as (
+    select 
+        t1.marketplace_key,
+        t1.asin,
+        t1.gl_product_group_desc,
+        t1.is_deal_purchase,
+        t1.brand_name,
+        t1.brand_code,
+        t1.product_category_desc,
+        -- t1.promotion_type,
+        -- t1.deal_first_available_datetime,
+        -- t1.event_name,
+        count(distinct t1.customer_id) as unique_customer_ct,
+        COALESCE(sum(t1.first_purchase_flag), 0) as first_purchase_ct,
+        COALESCE(sum(t1.one_mo_return_flag), 0) as one_mo_return_ct,
+        COALESCE(sum(t1.two_mo_return_flag), 0) as two_mo_return_ct,
+        COALESCE(sum(t1.third_mo_return_flag), 0) as third_mo_return_ct,
+        COALESCE(sum(t1.three_mo_plus_return_flag), 0) as third_mo_plus_return_ct,
         (
-            case 
-            when 
-                t1.customer_shipment_item_id NOT in (select customer_shipment_item_id from asin_promos_cp)
-                and t1.customer_purchase_id NOT in (select order_id from asin_promos_cp)
-            then 'Not Promo Purchase'
-            else 'Promo Purchase'
-            end 
-        ) as if_promo_flag
-    FROM order_metrics t1
+            COALESCE(sum(t1.one_mo_return_flag), 0) 
+            + COALESCE(sum(t1.two_mo_return_flag), 0) 
+            + COALESCE(sum(t1.third_mo_return_flag), 0) 
+            + COALESCE(sum(t1.three_mo_plus_return_flag), 0) 
+        ) AS return_purchase_ct,
+        sum(t1.shipped_units) as shipped_units,
+        sum(t1.revenue_share_amt) as revenue_share_amt,
+        sum(t1.display_ads_amt) as display_ads_amt,
+        sum(t1.subscription_revenue_amt) as subscription_revenue_amt
+        -- sum(t1.coupon_quantity) as promo_coupon_quantity
+    from asin_promos_cp t1
+    GROUP BY 
+        t1.marketplace_key,
+        t1.asin,
+        t1.gl_product_group_desc,
+        t1.is_deal_purchase,
+        t1.brand_name,
+        t1.brand_code,
+        t1.product_category_desc
+        -- t1.promotion_type,
+        -- t1.deal_first_available_datetime,
+        -- t1.event_name,
+         
 );
 
+-------------------------------------------------- BENCHMARKS --------------------------------------------------
+DROP TABLE IF EXISTS category_benchmark;
+CREATE TEMP TABLE category_benchmark as (
+    select
+        t1.marketplace_id,
+        t1.gl_product_group_desc,
+        t1.product_category_desc,
+        t1.is_deal_purchase,
+        count(distinct t1.customer_id) as unique_customer_ct,
+        sum(t1.first_purchase_ct) as first_purchase_ct,
+        sum(t1.one_mo_return_ct) as one_mo_return_ct, 
+        sum(t1.two_mo_return_ct) as two_mo_return_ct,
+        sum(t1.third_mo_return_ct) as third_mo_return_ct,
+        sum(t1.shipped_units) as shipped_units,
+        sum(t1.revenue_share_amt) as revenue_share_amt,
+        sum(t1.display_ads_amt) as display_ads_amt,
+        sum(t1.subscription_revenue_amt) as subscription_revenue_amt,
+        sum(t1.coupon_quantity) as promo_coupon_quantity
+    from asin_purchase t1
+    group by 
+        t1.marketplace_id,
+        t1.gl_product_group_desc,
+        t1.is_deal_purchase,
+        t1.product_category_desc
+);
 
+DROP TABLE IF EXISTS gl_benchmark;
+CREATE TEMP TABLE gl_benchmark as (
+    select
+        t1.marketplace_id,
+        t1.gl_product_group_desc,
+        t1.is_deal_purchase,
+        count(distinct t1.customer_id) as unique_customer_ct,
+        sum(t1.first_purchase_ct) as first_purchase_ct,
+        sum(t1.one_mo_return_ct) as one_mo_return_ct, 
+        sum(t1.two_mo_return_ct) as two_mo_return_ct,
+        sum(t1.third_mo_return_ct) as third_mo_return_ct,
+        sum(t1.shipped_units) as shipped_units,
+        sum(t1.revenue_share_amt) as revenue_share_amt,
+        sum(t1.display_ads_amt) as display_ads_amt,
+        sum(t1.subscription_revenue_amt) as subscription_revenue_amt,
+        sum(t1.coupon_quantity) as promo_coupon_quantity
+    from asin_purchase t1
+    group by 
+        t1.marketplace_id,
+        t1.gl_product_group_desc,
+        t1.is_deal_purchase
+        -- t1.product_category_desc
+);
 
 
 
@@ -270,7 +377,7 @@ CREATE TEMP TABLE flag_promo as (
 --         and t1.suppression_reason = '' or t1.suppression_reason = '[]' --test if this works
 -- );
 
-
+-------------------------------------------------- OUTPUT --------------------------------------------------
 -- DROP TABLE IF EXISTS pm_sandbox_aqxiao.new_to_brand_job_test2;
 -- CREATE TABLE pm_sandbox_aqxiao.new_to_brand_job_test2 AS (
 --     SELECT
